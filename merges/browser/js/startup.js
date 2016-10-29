91a3da5732f40009a859d2b760d476b55dbc5bd6
Alex Fedorov <fedor@anuta.org>
2016-10-29 10:14:51 +0200

    WIP. Migrating old app to the new structure.

---
+++ merges/browser/js/startup.js
@@ -0,0 +1,21 @@
+var app = {
+    initialize: function() {
+        console.log("Initialize");
+        this.bindEvents();
+        
+    },
+    bindEvents: function() {
+        console.log("BindEvents");
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady: function() {
+        console.log("On device ready");
+        app.receivedEvent('deviceready');
+    },
+    receivedEvent: function(id) {
+        console.log('Received Event: ' + id);
+    }
+};
+
+console.log("startup.js");
+app.initialize();
